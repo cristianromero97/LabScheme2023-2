5a34c6d0e976c0166d03be6aa405a2636f681472
cristianromero97 <cristian.romero.m@usach.cl>
2023-09-26 16:33:33 -0400

    Avance

--- lab.rkt
+++ lab.rkt
@@ -246,6 +246,110 @@
       "Error: Argumentos no válidos."))
 
 
+;DOM : system X message (string)
+;REC : system
+;Recursion : Cola
+;Resumen : Función que permite interactuar con un chatbot.
+(define (system-talk-rec sistema keyword)
+  (define (buscar-en-chatbot chatbot keyword)
+    (cond
+      ((null? chatbot) '()) ; Si no se encuentra en el chatbot actual
+      ((list? (car chatbot))
+       (let ((resultado (buscar-en-chatbot (car chatbot) keyword)))
+         (if (not (null? resultado))
+             resultado
+             (buscar-en-chatbot (cdr chatbot) keyword))))
+      ((string? (car chatbot))
+       (if (string=? (car chatbot) keyword)
+           chatbot ; Si se encuentra la opción, se devuelve el chatbot actual
+           (buscar-en-chatbot (cdr chatbot) keyword)))
+      (else (buscar-en-chatbot (cdr chatbot) keyword))))
+
+  (define (recursivo sistema keyword)
+    (cond
+      ((null? sistema) '("Opción no encontrada")) ; Si no se encuentra en ningún chatbot
+      ((list? sistema)
+       (let ((resultado (buscar-en-chatbot (car sistema) keyword)))
+         (if (not (null? resultado))
+             resultado
+             (recursivo (cdr sistema) keyword))))
+      (else (recursivo (cdr sistema) keyword))))
+
+  (let ((resultado (recursivo sistema keyword)))
+    (if (equal? resultado '("Opción no encontrada"))
+        resultado
+        (list
+        "Hola\n"        
+        "Interesante..... entonces planeas" (car resultado)
+        "Segun la base de datos tu proposito es" (cdr resultado)
+        "Por ende tu objetivo analizando lo anterior seria" (cddr resultado)
+        "Espero haberte ayudado..............."
+        )
+        )))
+
+;DOM : system X message (string)
+;REC : system
+;Recursion : Ninguna
+;Resumen : Función que permite interactuar con un chatbot. Mismo propósito de la función anterior pero con una implementación declarativa
+(define (system-talk-norec sistema keyword)
+  (let loop ((sistema sistema))
+    (cond
+      ((null? sistema) '("Opción no encontrada")) ; Si no se encuentra en ningún chatbot..
+      ((list? sistema)
+       (let* ((chatbot (car sistema))
+              (resultado (buscar-en-chatbot chatbot keyword)))
+         (if (not (null? resultado))
+             resultado
+             (loop (cdr sistema)))))
+      (else (loop (cdr sistema))))))
+
+;DOM : chatbot x string (keyword)
+;REC : chatbot
+;Recursion : Ninguna
+;Resumen : Función que busca al chatbot correspondiente para su recorrido en la funcion anterior.
+(define (buscar-en-chatbot chatbot keyword)
+  (cond
+    ((null? chatbot) '()) ; Si no se encuentra en el chatbot actual
+    ((list? (car chatbot))
+     (let ((resultado (buscar-en-chatbot (car chatbot) keyword)))
+       (if (not (null? resultado))
+           resultado
+           (buscar-en-chatbot (cdr chatbot) keyword))))
+    ((string? (car chatbot))
+     (if (string=? (car chatbot) keyword)
+         chatbot ; Si se encuentra la opción, se devuelve el chatbot actual
+         (buscar-en-chatbot (cdr chatbot) keyword)))
+    (else (buscar-en-chatbot (cdr chatbot) keyword))))
+
+
+;DOM :system X usuario (String)
+;REC : string
+;Recursion : Ninguna
+;Resumen : Función que ofrece una síntesis del chatbot para un usuario particular  a partir de chatHistory contenido dentro del sistema.
+(define (system-synthesis sistema usuario)
+  ; Verificar que el usuario esté dentro del sistema y haya iniciado sesión
+  (let ((logged-in (system-login sistema usuario)))
+    (if (string=? logged-in "Se ha ingresado a sesión correctamente.")
+        (let ((user-flow (find-user-flow sistema usuario)))
+          (if (not (null? user-flow))
+              (list "Contenido de la función:" user-flow)
+              "El usuario no tiene un flujo asignado."))
+        "El usuario no ha iniciado sesión o no está dentro del sistema.")))
+
+;DOM : system x user (String)
+;REC : system
+;Recursion : Ninguna
+;Resumen : Función que busca al usuario si esta dentro del sistema.
+(define (find-user-flow sistema usuario)
+  ; Buscar el flujo del usuario en el sistema
+  (if (null? sistema)
+      '()
+      (let ((current-user (car sistema)))
+        (if (string=? (car current-user) usuario)
+            (cdr current-user)
+            (find-user-flow (cdr sistema) usuario)))))
+
+
 ; EJEMPLO DE USO
 
 ;Para crear mas opciones se debe seguir el siguiente planteamiento
@@ -304,3 +408,15 @@
 ;Ejemplo de uso system-logout
 
 (define s444 (system-logout s3 "user1"))
+
+;Ejemplo de uso system-talk-rec
+(define s6 (system-talk-rec s1 "estudiar"))
+
+;Ejemplo de uso system-talk-norec
+(define s7 (system-talk-norec s1 "viajar"))
+
+; Ejemplo de uso system-synthesis
+(define s9 (system-add-user s1 "user0"))
+(define s10 (system-add-chatbot s9 cb11))
+(define s11 (system-synthesis s10 "user0"))
+
